607d7911a2fb60cc40ced299b8a77ed977ad8649
Roger8 <viomab@sina.com>
2019-10-23 10:20:31 +0800

    add camera & camera2 view demo

---
+++ camview2new/app/src/main/java/com/roger/cam/camera2basic/CameraActivity.java
@@ -0,0 +1,22 @@
+package com.roger.cam.camera2basic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.roger.cam.R;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+    }
+
+}
